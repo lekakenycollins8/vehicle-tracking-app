36922b58dec34b701bd1a1e5694e5d30e60917a9
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-11-13 12:48:08 +0300

    Initialize frontend and backend with React, Vite, and Express setup. Configured database connection

---
+++ backend/config/db.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '../.env' });
+
+// Encode special characters in the password
+
+const encodedPassword = encodeURIComponent(process.env.SUPABASE_DB_PASSWORD);
+
+// Connection string
+
+const connectionString = `postgres://${process.env.SUPABASE_DB_USER}:${encodedPassword}@${process.env.SUPABASE_DB_HOST}:${process.env.SUPABASE_DB_PORT}/${process.env.SUPABASE_DB_NAME}`;
+
+// Create a new instance of Sequelize
+
+const sequelize = new Sequelize(connectionString, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+// Database connection
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
+
+module.exports = sequelize;
