a330cebb36104a5c8032e9cb4729b9b40d9fba49
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-11-25 19:18:09 +0300

    feat: add Position model for vehicle tracking with geolocation and speed attributes

---
+++ backend/models/Position.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const Vehicle = require('./Vehicle');
+
+const Position = sequelize.define('Position', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    vehicleId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: Vehicle,
+            key: 'id'
+        }
+    },
+    latitude: {
+        type: DataTypes.DECIMAL(10, 8),
+        allowNull: false
+    },
+    longitude: {
+        type: DataTypes.DECIMAL(11, 8),
+        allowNull: false
+    },
+    speed: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    timestamp: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    attributes: {
+        type: DataTypes.JSON,
+        allowNull: true
+    }
+});
+
+Position.belongsTo(Vehicle);
+Vehicle.hasMany(Position);
+
+module.exports = Position;
