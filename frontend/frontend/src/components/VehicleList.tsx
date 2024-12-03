f0192c4c620e0e614cce675f91815cfbbf5e48ef
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-12-03 10:33:14 +0300

    fix: update import paths for axiosInstance and authActions in VehicleList and LoginForm components

--- frontend/frontend/src/components/VehicleList.tsx
+++ frontend/frontend/src/components/VehicleList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Table, Space, Tag } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { Vehicle } from '../types/vehicle';
-import axiosInstance from '../../axiosInstance';
+import axiosInstance from '../axiosInstance';
 
 interface VehicleListProps {
   onVehicleSelect: (vehicle: Vehicle) => void;
--- frontend/frontend/src/components/pages/LoginForm.tsx
+++ frontend/frontend/src/components/pages/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { login } from '../actions/authActions';
+import { login } from '../../authActions';
 import { Form, Input, Button, message } from 'antd';
 
 const LoginForm = () => {
